7a92692f7c1cb2a917b3362bc14f67aaa83cba72
alicedev94 <alicedev94@gmail.com>
2024-11-25 22:41:52 -0400

    All test check

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,16 +1,20 @@
 import { expect } from "chai";
 import sum from "../index.js";
-describe('sum', () => {
-    it('should return the sum of two numbers', () => {
-        const result = sum(2, 3);
-        expect(result).to.equal(5);
-    });
-    it('should handle negative numbers', () => {
-        const result = sum(-1, -1);
-        expect(result).to.equal(-2);
-    });
-    it('should handle zero', () => {
-        const result = sum(0, 0);
-        expect(result).to.equal(0);
-    });
+describe("sum", () => {
+  it("should return the sum of two numbers", () => {
+    const result = sum(2, 3);
+    expect(result).to.equal(5);
+  });
+  it("should handle negative numbers", () => {
+    const result = sum(-1, -1);
+    expect(result).to.equal(-2);
+  });
+  it("should handle zero", () => {
+    const result = sum(0, 0);
+    expect(result).to.equal(0);
+  });
+  it("should handle type float", () => {
+    const result = sum(1.10, 1.60);
+    expect(result).to.equal(2.70);
+  });
 });
