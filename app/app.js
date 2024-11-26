6db5434cebcf9a9af4d84b11d02b497288a572b2
alicedev94 <alicedev94@gmail.com>
2024-11-26 14:58:38 -0400

    rest api add

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send('All fine[MOCHAJS]'));
+
+app.listen(port, () => console.log('run in port', port));
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const sum = (n1, n2) => n1 + n2;
 export default sum;
+
